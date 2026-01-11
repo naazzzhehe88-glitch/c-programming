f325418f20093d0a4ce8c4433441088d9626ab12
naazzzhehe88-glitch <naazzzhehe88@gmail.com>
2026-01-11 12:19:00 +0530

    Create Smallest among 2 numbers.c

---
+++ Smallest among 2 numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter any two numbers\n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if (a>b)
+    printf("b is smallest");
+    else 
+    printf("a is smallest");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
