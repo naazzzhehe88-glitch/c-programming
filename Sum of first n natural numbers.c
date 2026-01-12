317b480301f1e4d69fecb77a794c2ca681acf209
naazzzhehe88-glitch <naazzzhehe88@gmail.com>
2026-01-12 13:41:14 +0530

    Create Sum of first n natural numbers.c

---
+++ Sum of first n natural numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter any natural number:");
+    scanf("%d", &n);
+    int sum=0;
+    for( int i=1; i<=n;i++)
+    {
+        sum = sum+i;
+        
+    }
+    printf("sum is %d \n", sum);
+    return 0;
+}
